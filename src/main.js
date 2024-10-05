4c0f94f2488b8b827674f242d15e32c8a9df7f3b
Hunter Tran <4468525+huntertran@users.noreply.github.com>
2024-10-05 15:34:14 -0400

    display data for 2014

--- src/main.js
+++ src/main.js
@@ -13,64 +13,100 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import { useGeographic } from 'ol/proj';
+import Papa from 'papaparse';
 
-var count = 20000;
-var features = new Array(count);
-var e = 4500000;
-for (var i = 0; i < count; ++i) {
-  var coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
-  features[i] = new Feature(new Point(coordinates));
+// Set coordinate system to [longitude, latitude]
+useGeographic();
+
+// Constants
+const DEFAULT_STROKE_COLOR = '#fff';
+const DEFAULT_FILL_COLOR = '#FF0000';
+const TEXT_FILL_COLOR = '#fff';
+const MAP_CENTER = [-79.3832, 43.6532];
+const INITIAL_ZOOM = 7;
+const styleCache = {};
+
+// Helper Functions
+function createStroke(color = DEFAULT_STROKE_COLOR) {
+  return new Stroke({ color });
 }
 
-var source = new VectorSource({
-  features: features
-});
+function createFill(color = DEFAULT_FILL_COLOR) {
+  return new Fill({ color });
+}
 
-var clusterSource = new Cluster({
-  distance: 40,
-  source: source
-});
+function createText(size) {
+  return new Text({
+    text: size.toString(),
+    fill: createFill(TEXT_FILL_COLOR)
+  });
+}
 
-var styleCache = {};
-var clusters = new VectorLayer({
-  source: clusterSource,
-  style: function (feature, resolution) {
-    var size = feature.get('features').length;
-    var style = styleCache[size];
-    if (!style) {
-      style = [new Style({
-        image: new CircleStyle({
-          radius: 10,
-          stroke: new Stroke({
-            color: '#fff'
-          }),
-          fill: new Fill({
-            color: '#3399CC'
-          })
-        }),
-        text: new Text({
-          text: size.toString(),
-          fill: new Fill({
-            color: '#fff'
-          })
-        })
-      })];
-      styleCache[size] = style;
-    }
-    return style;
-  }
-});
-
-var map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
+function createStyle(size) {
+  return new Style({
+    image: new CircleStyle({
+      radius: 10,
+      stroke: createStroke(),
+      fill: createFill()
     }),
-    clusters
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+    text: createText(size)
+  });
+}
+
+function getStyle(size) {
+  if (!styleCache[size]) {
+    styleCache[size] = createStyle(size);
+  }
+  return styleCache[size];
+}
+
+// Main Code
+var features = [];
+
+fetch('data/2014.csv')
+  .then(response => response.text())
+  .then(csvText => {
+    Papa.parse(csvText, {
+      header: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        results.data.forEach(row => {
+          var coordinates = [row.Longitude, row.Latitude];
+          features.push(new Feature(new Point(coordinates)));
+        });
+
+        var source = new VectorSource({
+          features: features
+        });
+
+        var clusterSource = new Cluster({
+          distance: 40,
+          source: source
+        });
+
+        var clusters = new VectorLayer({
+          source: clusterSource,
+          style: function (feature, resolution) {
+            var size = feature.get('features').length;
+            return getStyle(size);
+          }
+        });
+
+        var map = new Map({
+          target: 'map',
+          layers: [
+            new TileLayer({
+              source: new OSM(),
+            }),
+            clusters
+          ],
+          view: new View({
+            center: MAP_CENTER,
+            zoom: INITIAL_ZOOM,
+          }),
+        });
+      }
+    });
+  })
+  .catch(error => console.error('Error fetching the CSV file:', error));
